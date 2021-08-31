02714a2724f50b5db38bd3addbb7ee32d767a302
Matt Firmston <mattfirmston@gmail.com>
2021-08-31 12:31:35 +0100

    changed first test to not be reliant on unnecessary attribute reader

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,8 @@
 class BankAccount
-  attr_reader :balance
-  
+  def initialize
+    @balance = 0
+  end
+
   def deposit(amount)
     @balance = amount
   end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,8 +1,16 @@
 require 'bank_account'
 
 describe BankAccount do
-  it "should deposit an amount" do
-    subject.deposit(1000)
-    expect(subject.balance).to eq(1000)
+  describe "#deposit" do
+    it "should deposit an amount" do
+      subject.deposit(1000)
+      expect(subject.instance_variable_get(:@balance)).to eq(1000)
+    end
+  end
+
+  describe "balance" do
+    it "should have a balance of 0 when initialized" do
+      expect(subject.instance_variable_get(:@balance)).to eq(0)
+    end
   end
 end
